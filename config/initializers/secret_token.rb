3432c8448962c7f99318b9bbd186df373589f349
trollon <kueny.mickael@etu.unice.fr>
2012-10-05 11:22:09 +0200

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TP2App::Application.config.secret_token = '6b072cdd8fdce8fd9da208d92c8675b3d71ee9776fabe3e8de9b8e287b665d0d3061350fd9d2150258b043b5219e4fff202a9b64e77099cb63bde00c2ed02cdd'
